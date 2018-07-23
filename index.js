bee8e9f62f1a7a3966f8eeacbc5b510b5d06f328
Keebinz <35859881+Keebinz@users.noreply.github.com>
2018-07-23 16:56:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#that").hover(calc, calc)
+        
+    });
+
+
+function calc(){
+var T = 0;
+T = document.getElementById("that").value;
+console.log(T);
+
+
+
+
+
+}
